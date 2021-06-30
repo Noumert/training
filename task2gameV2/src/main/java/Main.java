36a165bf4e520da97e6a439b0629233ec7a2a4c0
vladmir.pavluchinskiy <vladmir.pavluchinskiy@gmail.com>
2021-06-30 13:47:02 +0300

    create methods for class Model

--- task2gameV2/src/main/java/Main.java
+++ task2gameV2/src/main/java/Main.java
@@ -2,9 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        //Model model = new Model();
+        Model model = new Model();
         View view = new View();
-        Controller controller = new Controller(new Model(), view);
+        Controller controller = new Controller(model, view);
 
         controller.processUser();
     }
