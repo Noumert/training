56f6970b6bd9b43acff5456aa75ad99e461206ec
vladmir.pavluchinskiy <vladmir.pavluchinskiy@gmail.com>
2021-07-06 13:46:01 +0300

    add task3inputFormV2_1

---
+++ task3inputFormV2_1/src/main/java/org/vova/Main.java
@@ -0,0 +1,13 @@
+package org.vova;
+import controller.Controller;
+import model.Model;
+import view.View;
+
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+        Controller controller = new Controller(new Model(), new View());
+        // Run
+        controller.processUser();
+    }
+}
