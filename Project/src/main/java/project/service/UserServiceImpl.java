d70aa55f3c0755684e3520e3eb62879b3ee4e10d
vladmir.pavluchinskiy <vladmir.pavluchinskiy@gmail.com>
2021-08-21 22:55:23 +0300

    delete useless exception

--- Project/src/main/java/project/service/UserServiceImpl.java
+++ Project/src/main/java/project/service/UserServiceImpl.java
@@ -1,18 +1,14 @@
 package project.service;
 
-import javassist.NotFoundException;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import project.entity.MyUserDetails;
 import project.entity.RoleType;
 import project.entity.User;
-import project.exceptions.DuplicatedEmailException;
 import project.repository.UserRepository;
 
 import java.util.*;
